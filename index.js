7bc331ca7a8a8a16a59f7ef875a595b9dbbf79c7
lauhar <48447189+LauriSakari@users.noreply.github.com>
2021-06-15 10:56:25 +0200

    uibuild

--- index.js
+++ index.js
@@ -15,52 +15,31 @@ morgan.token('tiedot', (req, res) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :tiedot'))
 
-let persons = [
-      { 
-        "name": "Arto Hellas", 
-        "number": "040-123456",
-        "id": 1
-      },
-      { 
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      { 
-        "name": "Dan Abramov", 
-        "number": "12-43-234345",
-        "id": 3
-      },
-      { 
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122",
-        "id": 4
-      }
-    ]
-  
-
-app.get('/info', (req, res) => {
-    res.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${Date()}`)
+app.get('/info', (request, response) => {
+    Person.find({}).then(persons => {
+        response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${Date()}`)
+    })
 })
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
-        res.json(persons)
+        response.json(persons)
     })
 })
 
 
-app.get('/api/persons/:id', (req, res) =>   {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) { 
-        res.json(person)    
-        } else { 
-         res.status(404).end()
+app.get('/api/persons/:id', (request, response, next) =>   {
+    Person.findById(request.params.id)
+    .then(person => {
+        if (person) {
+        response.json(person)
+        } else {
+            response.status(404).end()
         }
+    })
+    .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -70,45 +49,71 @@ const generateId = () => {
     return maxId + 5 + Math.floor(Math.random() * 20)
 }
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (request, response, next) => {
     
-    const body = req.body
-    //NIMIEN VERTAILU
-    // const names = persons.map(p => p.name.toLowerCase())
-    // const nameToCompare = body.name.toLowerCase()
-
+    const body = request.body
     
     if (!body.name || !body.number)  {
-        return res.status(400).json({
+        return response.status(400).json({
             error: 'content missing'
         })
     }
-    //SAMA NIMI ERROR
-    // } else if (names.includes(nameToCompare) ){
-    //     return res.status(400).json({
-    //         error: 'name must be unique'
-    //     })
-    // }
 
     const person = new Person({
         name: body.name,
         number: body.number,
     })
 
-    // PERSONS LISTAAN LISAYS
-    // persons = persons.concat(person)
     person.save().then(savedPerson => {
-        res.json(savedPerson)
+        response.json(savedPerson)
     })
+    .catch(error => next(error))
   })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id )
-
-    res.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+    
+    Person.findByIdAndUpdate(request.params.id, person, {new:true})
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.log('error console')
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name = 'ValidationError')  {
+        return response.status(400).json({error: error.message})
+    }
+  
+    next(error)
+  }
+   // tämä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen!
+app.use(errorHandler)
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  // olemattomien osoitteiden käsittely
+  app.use(unknownEndpoint)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose') 
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
@@ -12,8 +13,8 @@ mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindA
 })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: {type: String, required: true, unique: true, minlength: 3},
+    number: {type: String, minlength: 8}
 })
 
 personSchema.set('toJSON', {
